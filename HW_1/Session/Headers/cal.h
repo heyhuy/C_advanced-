7862eb94e23507bb580168556837e1e896dc5a35
Huy Phạm <50261634+heyhuy@users.noreply.github.com>
2022-05-08 17:31:59 +0700

    Add files via upload

---
+++ HW_1/Session/Headers/cal.h
@@ -0,0 +1,7 @@
+#ifndef CALC_H_INCLUDED
+#define CALC_H_INCLUDED
+int sum (int a[], int n);
+int TimX(int a[], int b[], int n,int y);
+int Max(int a[], int n);
+int Min(int a[], int n);
+#endif 
---
+++ HW_1/Session/Headers/convert.h
@@ -0,0 +1,5 @@
+#ifndef CONVERNT_H_INCLUDED
+#define CONVERNT_H_INCLUDED
+int DectoBinary(int n);
+int DectoHex(int xyz);
+#endif 
